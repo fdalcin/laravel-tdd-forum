128c8e973c8f0b78a34c7a7be9af5687dfaed5a7
Felipe Dalcin <felipe.dalcin@outlook.com>
2018-02-28 17:02:35 -0300

    Adding form to thread view so the auth user can post a reply.

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -24,5 +24,23 @@
         @endforeach
       </div>
     </div>
+
+    @if(auth()->check())
+      <div class="row justify-content-center">
+        <div class="col-md-8 mt-4">
+          <form method="POST" action="{{ $thread->path() . '/replies' }}">
+            {{ csrf_field() }}
+
+            <div class="form-group">
+              <textarea name="body" id="body" class="form-control" placeholder="Have something to say?" rows="5"></textarea>
+            </div>
+
+            <button type="submit" class="btn btn-primary">Post</button>
+          </form>
+        </div>
+      </div>
+    @else
+      <p class="justify-content-center text-center mt-4">Please <a href="{{ route('login') }}">sing in</a> to participate in this discussion.</p>
+    @endif
 </div>
 @endsection
