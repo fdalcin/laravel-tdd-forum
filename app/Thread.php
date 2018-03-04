0249dcb92a552a4e06093ab5a709ee3e2c26a31e
Felipe Dalcin <felipe.dalcin@outlook.com>
2018-03-04 09:07:08 -0300

    Optimizing queries part 2; Extracting favorites functions to a trait;

--- app/Thread.php
+++ app/Thread.php
@@ -8,6 +8,8 @@ class Thread extends Model
 {
     protected $guarded = [];
 
+    protected $with = ['channel', 'owner'];
+
     public static function boot()
     {
         parent::boot();
@@ -44,8 +46,6 @@ class Thread extends Model
 
     public function replies()
     {
-        return $this->hasMany(Reply::class)
-            ->with('owner')
-            ->withCount('favorites');
+        return $this->hasMany(Reply::class);
     }
 }
