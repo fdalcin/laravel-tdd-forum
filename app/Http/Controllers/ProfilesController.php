39c625330de51a7e2b52065092e47dd697ba5e8f
Felipe Dalcin <felipe.dalcin@outlook.com>
2018-03-05 16:21:01 -0300

    Extracting a Controller Query to the Model

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Activity;
 use App\User;
 use Illuminate\Http\Request;
 
@@ -10,8 +11,8 @@ class ProfilesController extends Controller
     public function show(User $user)
     {
         return view('profiles.show', [
-            'user'    => $user,
-            'threads' => $user->threads()->paginate(30),
+            'user' => $user,
+            'activities' => Activity::feed($user),
         ]);
     }
 }
--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -6,30 +6,17 @@
             <div class="col-md-8">
                 <div class="page-header mb-4">
                     <h1>{{ $user->name }}</h1>
-
-                    <small>Since {{ $user->created_at->diffForHumans() }}</small>
                 </div>
 
-                @foreach($threads as $thread)
-                    <div class="card mb-4">
-                        <div class="card-header">
-                            <div class="level">
-                                <span class="flex">
-                                    <a href="{{ route('profile', $thread->owner) }}">{{ $thread->owner->name }}</a> posted:
-                                    <a href="{{ $thread->path() }}">{{ $thread->title }}</a>
-                                </span>
-
-                                <span>{{ $thread->created_at->diffForHumans() }}</span>
-                            </div>
-                        </div>
-
-                        <div class="card-body">
-                            {{ $thread->body }}
-                        </div>
+                @foreach($activities as $date => $activity)
+                    <div class="page-header mb-2">
+                        <h4>{{ $date }}</h4>
                     </div>
-                @endforeach
 
-                {{ $threads->links() }}
+                    @foreach($activity as $record)
+                        @include('profiles.activities.' . $record->type, ['activity' => $record])
+                    @endforeach
+                @endforeach
             </div>
         </div>
     </div>
