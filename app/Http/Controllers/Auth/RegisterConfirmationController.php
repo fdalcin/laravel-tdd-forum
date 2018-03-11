5ef58654544f3228b91f2facbfa141f7107791e2
Felipe Dalcin <felipe.dalcin@outlook.com>
2018-03-11 08:45:44 -0300

    Confirmation token Loose ends

--- app/Http/Controllers/Auth/RegisterConfirmationController.php
+++ app/Http/Controllers/Auth/RegisterConfirmationController.php
@@ -4,19 +4,19 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\User;
-use Illuminate\Http\Request;
 
 class RegisterConfirmationController extends Controller
 {
     public function index()
     {
-        try {
-            User::where('confirmation_token', request('token'))
-                ->firstOrFail()
-                ->confirm();
-        } catch (\Exception $e) {
+        $user = User::where('confirmation_token', request('token'))
+            ->first();
+
+        if (! $user) {
             return redirect(route('threads.index'))->with('flash', 'Unknown token');
         }
+
+        $user->confirm();
 
         return redirect(route('threads.index'))
             ->with('flash', 'Your account is now confirmed.');
--- tests/Feature/RegistrationTest.php
+++ tests/Feature/RegistrationTest.php
@@ -49,7 +49,11 @@ class RegistrationTest extends TestCase
             route('register.confirm', ['token' => $user->confirmation_token])
         )->assertRedirect(route('threads.index'));
 
-        $this->assertTrue($user->fresh()->confirmed);
+        tap($user->fresh(), function ($user) {
+            $this->assertTrue($user->confirmed);
+            $this->assertNull($user->confirmation_token);
+        });
+
     }
 
     /** @test */
